64d4a4929bd8e46bda8d6442b5d0529c17aacb43
Jeon JaeSeung <wotmd6180@gmail.com>
2020-09-07 14:05:12 +0900

    09.07 Update

---
+++ College_2ndYear/Network/NetworkReview/DNSProgram/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net;
+
+class DNSName
+{
+    public static void Main()
+    {
+        string hostName = Dns.GetHostName();
+        Console.WriteLine("Local hostname : {0}", hostName);
+
+        IPHostEntry myself = Dns.GetHostEntry(hostName);
+        foreach (IPAddress address in myself.AddressList)
+        {
+            Console.WriteLine("IP Address : {0}", address.ToString());
+        }
+    }
+}
---
+++ College_2ndYear/Network/NetworkReview/ThreadSync/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    private int number = 0;
+
+    static void Main(string[] args)
+    {
+        Program pg = new Program();
+
+        Thread t1 = new Thread(threadFunc);
+        Thread t2 = new Thread(threadFunc);
+
+        t1.Start(pg);
+        t2.Start(pg);
+
+        t1.Join();
+        t2.Join();
+
+        Console.WriteLine(pg.number);
+    }
+
+    static void threadFunc(object inst)
+    {
+        Program pg = inst as Program;
+        if (pg != null)
+        {
+            for (int i = 0; i < 10000; ++i)
+            {
+                Interlocked.Increment(ref pg.number);
+            }
+        }
+    }
+}
